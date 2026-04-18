12fcf19b6bdae88ba917e5d46ab4f547bbdaee82
stellorbit <76532335+stellorbit@users.noreply.github.com>
2026-04-19 02:53:18 +0900

    remark/rehype プラグイン追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://stellorbit.net',
-	integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [
+      [remarkToc, { heading: '目次', tight: true }],
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: 'append' }],
+    ],
+  },
 });
